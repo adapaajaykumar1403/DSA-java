8e96cc0109d8ab1e4d21ea1b531b47339f7bd3e3
adapaajaykumar1403 <aadapaajay@gmail.com>
2025-12-19 16:18:05 +0530

    Update combinationSum.java

--- combinationSum.java
+++ combinationSum.java
@@ -1,3 +1,4 @@
+//Method -->1using for loop (own)
 class Solution {
     public List<List<Integer>> ans= new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -25,5 +26,34 @@ class Solution {
             generateComb(candidates,target, sum+candidates[i],i,helper);
             helper.remove(helper.size()-1);
         }
+    }
+}
+//Method --> not using for loop adding it or not adding the element at the current index(accio)
+class Solution {
+    public List<List<Integer>> ans= new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        generateComb(candidates,target,0,0,new ArrayList<Integer>());
+        return ans;
+        
+    }
+    public void generateComb(int[] candidates,int target,int sum,int ind,ArrayList<Integer> helper)
+    {
+        if(sum==target)
+        {
+            List<Integer> li=new ArrayList<>(helper);
+            ans.add(li);
+            return;
+        }
+        if(sum>target || ind>=candidates.length)
+        {
+            return;
+        }
+
+        
+            helper.add(candidates[ind]);
+            generateComb(candidates,target, sum+candidates[ind],ind,helper);
+            helper.remove(helper.size()-1);
+            generateComb(candidates,target,sum,ind+1,helper);
     }
 }
