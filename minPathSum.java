36d9af53be6d94c5a435c856470ebae5c775050c
adapaajaykumar1403 <aadapaajay@gmail.com>
2025-12-28 01:11:06 +0530

    Update minPathSum.java

--- minPathSum.java
+++ minPathSum.java
@@ -39,3 +39,61 @@ class Solution {
 
     }
 }
+//method 2--------------------------
+class Solution {
+    public int min=Integer.MAX_VALUE;
+    public int minPathSum(int[][] grid) {
+        int[][] dp=new int[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
+        int ans= minTab(grid,grid.length-1,grid[0].length-1,dp);
+        return ans;
+       // return min(grid,grid.length-1,grid[0].length-1,dp);
+        
+    }
+    public int minTab(int[][]grid,int cr,int cc,int[][]dp)
+    {
+        //dp[cr][cc]=grid[cr][cc];
+        for(int i=cc;i>=0;i--)
+        {
+            for(int j=cr;j>=0;j--)
+            {
+                if(i==cc && j==cr)
+                {
+                    dp[j][i]=grid[j][i];
+                    continue;
+                }
+                dp[j][i]=Math.min((j+1<=cr?dp[j+1][i]:Integer.MAX_VALUE),(i+1<=cc?dp[j][i+1]:Integer.MAX_VALUE))+grid[j][i];
+            }
+        }
+        return dp[0][0];
+    }
+    public int min(int[][] grid,int cr, int cc,int[][]dp)
+    {
+    
+        if(cr==grid.length-1 && cc==grid[0].length-1)
+        {
+            dp[cr][cc]=grid[cr][cc];
+            return grid[cr][cc];
+        }
+        if(cr>=grid.length || cc>=grid[0].length)
+        {
+            return Integer.MAX_VALUE;
+        }
+        if(dp[cr][cc]!=-1)
+        {
+            return dp[cr][cc];
+        }
+        int path1=min(grid,cr,cc+1,dp);
+        int path2=min(grid,cr+1,cc,dp);
+        dp[cr][cc]=(Math.min(path1,path2))+grid[cr][cc];
+        return (Math.min(path1,path2))+grid[cr][cc];
+
+
+    }
+}
